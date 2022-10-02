afdaabe30f639bb48f701c243bbbedc9befa5a71
Johannes Skuterud <johann1301s@icloud.com>
2022-10-02 20:37:28 +0200

    wip

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,35 @@
+import React, { useEffect } from 'react'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
+const ROTIDER = [
+  {
+    'time': "2:30",
+    'meter': 1250
+  },
+  {
+    'time': "2:10",
+    'meter': 1050
+  },
+  {
+    'time': "2:40",
+    'meter': 1450
+  },  {
+    'time': "2:50",
+    'meter': 1250
+  }
+];
+
 export default function Home() {
+  const [maxMeter, setMaxMeter] = React.useState(0);
+
+  
+
+  const filteredRowingTimes = () => {
+    return ROTIDER.filter(({time,meter}) => meter <= maxMeter)
+  }
+
   return (
     <div className={styles.container}>
       <Head>
@@ -21,35 +48,22 @@ export default function Home() {
           <code className={styles.code}>pages/index.js</code>
         </p>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+        <div>
+          <h2>Ro tider</h2>
+          <label>Maksmeter </label>
+          <input type="text" name="maxMeter" placeholder="Skriv inn meter" 
+            onChange={(event) => {
+              setMaxMeter(parseInt(event.target.value))
+            }}
+          />
+          <p>Nåværende: {maxMeter} meter</p>
+          <ul>
+            {filteredRowingTimes().map((roTid) => 
+              <li key={roTid.time}>{roTid.time} - {roTid.meter} meter</li>
+            )}
+          </ul>
         </div>
+
       </main>
 
       <footer className={styles.footer}>
